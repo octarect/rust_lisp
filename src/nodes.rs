7a130e49f313ad9bc98e2c689982d0910f54f2b9
rk0der <ryo.alter@gmail.com>
2017-01-29 03:06:26 +0900

    rename OpNode to CallNode and change its structure

--- src/nodes.rs
+++ src/nodes.rs
@@ -14,11 +14,10 @@ pub enum Operator {
 }
 
 pub enum Node {
-  OpNode {
+  CallNode {
     header: NodeHeader,
     op: Operator,
-    left: Box<Node>,
-    right: Box<Node>,
+    args: Vec<Node>,
   },
   ValueNode {
     header: NodeHeader,
@@ -27,14 +26,14 @@ pub enum Node {
 }
 
 impl Node {
-  pub fn eval(self) -> DataType {
-    match self {
-      Node::OpNode {op, left, right, ..} => {
-        match op {
-          Operator::Add => left.eval() + right.eval(),
-          Operator::Sub => left.eval() - right.eval(),
-          Operator::Mul => left.eval() * right.eval(),
-          Operator::Div => left.eval() / right.eval(),
+  pub fn eval(&self) -> DataType {
+    match *self {
+      Node::CallNode {ref op, ref args, ..} => {
+        match *op {
+          Operator::Add => args[0].eval() + args[1].eval(),
+          Operator::Sub => args[0].eval() - args[1].eval(),
+          Operator::Mul => args[0].eval() * args[1].eval(),
+          Operator::Div => args[0].eval() / args[1].eval(),
           _ => DataType::Int(-1),
         }
       },
@@ -42,11 +41,10 @@ impl Node {
     }
   }
   pub fn op(op: Operator, l: Node, r: Node, pos: u16) -> Node {
-    Node::OpNode {
+    Node::CallNode {
       header: NodeHeader {lineno: pos},
       op: op,
-      left: Box::new(l),
-      right: Box::new(r),
+      args: vec![l, r],
     }
   }
   pub fn value(val: DataType, pos: u16) -> Node {
